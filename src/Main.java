f7bf26a1526087fba5eab4a4bc22fb8987178bf3
dasha <dasha.motry2002@gmail.ru>
2024-07-10 00:57:08 +0300

    в процессе

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+    Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Малюткина Екатерина Борисовна", 1, 14_000);
+        employees[1] = new Employee("Крыскина Милана Рафаэльевна", 2, 12_856);
+        employees[2] = new Employee("Рупунцелева Лариса Ильинична", 3, 10_000);
+        employees[3] = new Employee("Черепашковна Кристина Микиландежловна", 4, 13_000);
+        employees[4] = new Employee("Кимчинова Любовь Ивановна", 5, 12_000);
+//        employees[5] = new Employee();
+//        employees[6] = new Employee();
+//        employees[7] = new Employee();
+//        employees[8] = new Employee();
+//        employees[9] = new Employee();
 
 
     }
+
+public void salarySum() {
+
+}
 }
